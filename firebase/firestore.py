9ddfe48e8a0fd3c181c8f1cad489693d747da801
Mantequilla-Innovacion <yahir.ponce@softtek.com>
2025-03-22 18:09:10 -0600

    Update firestore.py

--- firebase/firestore.py
+++ firebase/firestore.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase/mantequilla-python-firebase-adminsdk-fbsvc-02f3c40f1f.json")
+load_dotenv()
+
+cred = credentials.Certificate(os.getenv("FIRE_STORE_PATH_CRED"))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
